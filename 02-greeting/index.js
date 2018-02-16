9da448681b36d7ec5fce077d4d05da73d798fa40
Gustavo Morales <gustavo.morales@gmail.com>
2018-02-15 20:51:14 -0500

    02 Greeting

---
+++ 02-greeting/index.js
@@ -0,0 +1,14 @@
+var moment = require("moment");
+var colors = require("colors/safe");
+
+// Get hour number in 24 format
+var hour = moment().hour();
+
+// Ask for hours range
+if (hour >= 6 && hour < 12) {
+  console.log(colors.blue("Good morning"));
+} else if (hour >= 12 && hour < 18) {
+  console.log(colors.yellow("Good morning"));
+} else if (hour >= 18 && hour < 23) {
+  console.log(colors.gray("Good evening"));
+}
