5018ab926a7c907d3273edbe1dd481a817c3b3e9
Gustavo Morales <gustavo.morales@gmail.com>
2018-05-14 21:03:04 -0500

    Web Server Dynamic Exercise

---
+++ 09-web-server-dynamic/index.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const pug = require('pug');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const path = url.parse(req.url, true);
+  const name = path.pathname.substring(0, path.pathname.lastIndexOf('.'));
+  const pathName = `${__dirname}/views${name}.pug`;
+
+  fs.exists(pathName, function (exists) {
+    if (!exists) {
+      res.statusCode = 404;
+      res.setHeader('Content-Type', 'text/plain');
+      res.write('404 Not Found');
+      res.end();
+      return;
+    }
+
+    fs.readFile(pathName, 'utf-8', (err, file) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      const html = pug.render(file, {
+        pageTitle: 'Our great Website',
+        menu: [{
+          name: 'Home',
+          link: 'index.html'
+        }, {
+          name: 'About',
+          link: 'about.html'
+        }]
+      });
+      res.end(html);
+    });
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
