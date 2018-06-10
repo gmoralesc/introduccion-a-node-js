7d1a373935ed8483b1163ffecfaafe2d7fcca5a7
Gustavo Morales <gustavo.morales@gmail.com>
2018-06-10 08:29:28 -0500

    Update Bucket List using module path

--- 04-bucket-list/index.js
+++ 04-bucket-list/index.js
@@ -1,28 +1,30 @@
 const fs = require('fs');
 const os = require('os');
-const commandLineArgs = require('command-line-args')
+const path = require('path');
+const commandLineArgs = require('command-line-args');
 
+const filename = 'data.txt';
+const filepath = `${path.resolve('.')}/${filename}`;
 const params = [{
     name: 'item',
     alias: 'i',
     type: String
   },
   {
-    name: 'frequency',
-    alias: 'f',
-    type: String
-  },
-  {
     name: 'times',
     alias: 't',
     type: Number
+  },
+  {
+    name: 'frequency',
+    alias: 'f',
+    type: String
   }
 ];
-
 const options = commandLineArgs(params);
+
 const line = `You want to ${options.item} ${options.times} time(s) every ${options.frequency}`;
+fs.appendFileSync(filepath, line + os.EOL);
 
-fs.appendFileSync('data.txt', line + os.EOL);
-
-const content = fs.readFileSync('data.txt', 'utf-8');
+const content = fs.readFileSync(filepath, 'utf-8');
 console.log(content);
