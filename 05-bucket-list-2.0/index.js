da07a359b5b316ac7b8683fcf69a18402509b410
Gustavo Morales <gustavo.morales@gmail.com>
2018-06-10 08:41:05 -0500

    update Bucket List 2.0 to add path module

--- 05-bucket-list-2.0/index.js
+++ 05-bucket-list-2.0/index.js
@@ -1,36 +1,35 @@
 const fs = require('fs');
-const commandLineArgs = require('command-line-args')
+const path = require('path');
+const commandLineArgs = require('command-line-args');
 
+const filename = 'data.json';
+const filepath = `${path.resolve('.')}/${filename}`;
 const params = [{
     name: 'item',
     alias: 'i',
     type: String
   },
   {
+    name: 'times',
+    alias: 't',
+    type: Number
+  },
+  {
     name: 'frequency',
     alias: 'f',
     type: String
   },
-  {
-    name: 'times',
-    alias: 't',
-    type: Number
-  }
 ];
-
 const options = commandLineArgs(params);
-
-let content = '';
 let items = [];
-const filePath = 'data.json';
 
 try {
   //if file exist
-  content = fs.readFileSync(filePath, 'utf-8');
+  const content = fs.readFileSync(filepath, 'utf-8');
   items = JSON.parse(content);
 } catch (e) {
   //if file does not exist
-  fs.openSync(filePath, 'w');
+  fs.openSync(filepath, 'w');
 }
 
 items.push({
@@ -38,8 +37,7 @@ items.push({
   times: options.times,
   frequency: options.frequency
 });
-
-fs.writeFileSync(filePath, JSON.stringify(items, null, 2));
+fs.writeFileSync(filepath, JSON.stringify(items, null, 2));
 
 items.forEach(function (el) {
   console.log(`You want to ${el.item} ${el.times} time(s) in a ${el.frequency}`);
