6efe202f566a825ea8394bc43c4816355667b069
Gustavo Morales <gustavo.morales@gmail.com>
2018-06-12 07:07:42 -0500

    Update Exercises including path module

--- 08-web-server/index.js
+++ 08-web-server/index.js
@@ -1,15 +1,16 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const path = url.parse(req.url, true);
-  const pathName = `${__dirname}/public${path.pathname}`;
+  const pathurl = url.parse(req.url, true);
+  const filepath = `${path.resolve('.')}/public${pathurl.pathname}`;
 
-  fs.readFile(pathName, 'utf-8', (err, file) => {
+  fs.readFile(filepath, 'utf-8', (err, file) => {
     if (err) {
       res.statusCode = 404;
       res.setHeader('Content-Type', 'text/plain');
--- 09-web-server-2.0/index.js
+++ 09-web-server-2.0/index.js
@@ -1,17 +1,18 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const path = require('path');
 const pug = require('pug');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const path = url.parse(req.url, true);
-  const name = path.pathname.substring(0, path.pathname.lastIndexOf('.'));
-  const pathName = `${__dirname}/views${name}.pug`;
+  const pathurl = url.parse(req.url, true);
+  const filename = path.basename(pathurl.pathname, '.html');
+  const filepath = `${path.resolve('.')}/views/${filename}.pug`;
 
-  fs.readFile(pathName, 'utf-8', (err, file) => {
+  fs.readFile(filepath, 'utf-8', (err, file) => {
     if (err) {
       res.statusCode = 404;
       res.setHeader('Content-Type', 'text/plain');
