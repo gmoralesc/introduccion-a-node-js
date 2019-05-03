049d5de0fe6f147e2537696b176247b10ffdd71c
Gustavo Morales <gustavo.morales@gmail.com>
2019-05-03 06:00:10 -0500

    Fix typo

--- 02-greeting/index.js
+++ 02-greeting/index.js
@@ -8,7 +8,7 @@ const hour = format(new Date(), "H");
 if (hour >= 6 && hour < 12) {
   console.log(colors.blue("Good morning"));
 } else if (hour >= 12 && hour < 18) {
-  console.log(colors.yellow("Good morning"));
+  console.log(colors.yellow("Good afternoon"));
 } else if (hour >= 18 && hour < 23) {
   console.log(colors.gray("Good evening"));
 }
