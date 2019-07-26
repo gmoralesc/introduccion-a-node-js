df4cd1686112c0b228854f5ad87c6e5313559185
Gustavo Morales <gustavo.morales@gmail.com>
2019-07-26 13:49:38 -0500

    Update Web Server Exaples to use path.join

--- 08-web-server/index.js
+++ 08-web-server/index.js
@@ -1,28 +1,28 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   const pathurl = url.parse(req.url, true);
-  const filepath = `${path.resolve('.')}/public${pathurl.pathname}`;
 
-  fs.readFile(filepath, 'utf-8', (err, file) => {
+  const filepath = path.join(__dirname, "public", pathurl.pathname);
+
+  fs.readFile(filepath, "utf-8", (err, file) => {
     if (err) {
       res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.write('404 Not Found');
+      res.setHeader("Content-Type", "text/plain");
+      res.write("404 Not Found");
       res.end();
       return;
     }
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader("Content-Type", "text/html");
     res.end(file);
   });
-
 });
 
 server.listen(port, hostname, () => {
--- 09-web-server-2.0/index.js
+++ 09-web-server-2.0/index.js
@@ -1,36 +1,39 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-const path = require('path');
-const pug = require('pug');
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
+const pug = require("pug");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   const pathurl = url.parse(req.url, true);
-  const filename = path.basename(pathurl.pathname, '.html');
-  const filepath = `${path.resolve('.')}/views/${filename}.pug`;
+  const filename = path.basename(pathurl.pathname, ".html");
+  const filepath = path.join(__dirname, "views", `${filename}.pug`);
 
-  fs.readFile(filepath, 'utf-8', (err, file) => {
+  fs.readFile(filepath, "utf-8", (err, file) => {
     if (err) {
       res.statusCode = 404;
-      res.setHeader('Content-Type', 'text/plain');
-      res.write('404 Not Found');
+      res.setHeader("Content-Type", "text/plain");
+      res.write("404 Not Found");
       res.end();
       return;
     }
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
+    res.setHeader("Content-Type", "text/html");
     const html = pug.render(file, {
-      pageTitle: 'Our great Website',
-      menu: [{
-        name: 'Home',
-        link: 'index.html'
-      }, {
-        name: 'About',
-        link: 'about.html'
-      }]
+      pageTitle: "Our great Website",
+      menu: [
+        {
+          name: "Home",
+          link: "index.html",
+        },
+        {
+          name: "About",
+          link: "about.html",
+        },
+      ],
     });
     res.end(html);
   });
